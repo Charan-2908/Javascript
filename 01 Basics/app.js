7c279b90af3d57c339aee297e503ac8a3775b6dc
Gujjari Charan Kumar <cg600281@gmail.com>
2024-02-03 13:45:08 +0530

    updated

--- 01 Basics/app.js
+++ 01 Basics/app.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+document.write("Hello Javascript");
+// alert("Hello World");
+console.log("Hello People");
+console.log({name: "john"})
+
+
+let myName = "Charan"
+console.log(myName);
+console.log(myName);
+console.log(myName);
+
+let address, zip, state
+address = "LB nagar"
+zip = "500074"
+state = "TS"
+
+console.log(address,state,zip);
+
+myName = "Chintu"
+console.log(myName);
+
+let _random123_$ = "random"
+console.log(_random123_$);
+
+const lastName = "John"
+console.log(lastName);
+
+const firstName = "Holaa"
+const last_name = "Peter"
+let address_1 = "main street"
+
+address_1 = 'first street'
+
+console.log(firstName, last_name, address_1);
+console.log(firstName);
+console.log(last_name);
+console.log(address_1);
+
+const yup = 'Charan\'s courses are the best'
+const yupp = "Charan's courses are the best"
+console.log(yup);
+console.log(yupp);
+
