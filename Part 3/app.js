949292e98114d93d667dcc05629dc9a3af241d16
Gujjari Charan Kumar <cg600281@gmail.com>
2024-02-06 23:10:02 +0530

    Updated

---
+++ Part 3/app.js
@@ -0,0 +1,16 @@
+document.write('Hello')
+
+const people = [
+    {name: 'bob', age: 20, position: 'developer'},
+    {name: 'peter', age: 30, position: 'designer'},
+    {name: 'susy', age: 24, position: 'manager'}
+]
+
+people.forEach(function(item) {
+    console.log(item.position.toUpperCase());
+})
+
+function showPerson(people) {
+    console.log(people.position.toUpperCase());
+}
+people.forEach(showPerson)
