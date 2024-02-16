1a9d64b45ab8cd7c76b576282856eddff11d3d51
Gujjari Charan Kumar <cg600281@gmail.com>
2024-02-17 00:40:17 +0530

    updated

--- Part 3/app.js
+++ Part 3/app.js
@@ -29,5 +29,36 @@ document.body.innerHTML = names.join('')
 console.log(names);
 
 const youngPeople = people.filter(function(person){
-
+    return person.age < 30
 })
+console.log(youngPeople);
+
+const developers = people.filter(function(person){
+    return person.position === 'developer'
+})
+console.log(developers);
+
+const person = people.find(function(person){
+    return person.age < 30
+})
+console.log(person);
+
+const names1 = ['bob', 'peter', 'susy']
+console.log(names1.find(function(names){
+    return names == 'peter'
+}));
+
+
+const employee = [
+    { name: "bob", age: 20, position: "developer", salary: 300 },
+    { name: "peter", age: 30, position: "designer", salary: 500 },
+    { name: "susy", age: 24, position: "manager", salary: 500 },
+];
+
+const total = employee.reduce(function(acc, curr){
+    console.log(`total ${acc}`);
+    console.log(`current money: ${curr.salary}`);
+    acc += curr.salary
+    return acc
+},0)
+console.log(total);
