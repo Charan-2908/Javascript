f2e3218b86e09d71156ade1afb16c4512c1c1e4e
Gujjari Charan Kumar <cg600281@gmail.com>
2024-02-18 22:19:47 +0530

    Updated

---
+++ Part 3/array.js
@@ -0,0 +1,32 @@
+const students = [
+    {
+        id: 1,
+        name: "peter",
+        score: 80,
+        favSub: "math",
+    },
+    {
+        id: 2,
+        name: "bob",
+        score: 70,
+        favSub: "chemistry",
+    },
+    {
+        id: 3,
+        name: "anna",
+        score: 80,
+        favSub: "math",
+    },
+    {
+        id: 4,
+        name: "susy",
+        score: 34,
+        favSub: "social",
+    },
+    {
+        id: 5,
+        name: "john",
+        score: 87,
+        favSub: "math",
+    },
+];
